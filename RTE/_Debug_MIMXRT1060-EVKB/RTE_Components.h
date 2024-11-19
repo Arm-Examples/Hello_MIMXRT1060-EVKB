1b2c01bc5c8726be7311960f3694d8bd5312f1c0
David Lesnjak <david.lesnjak@arm.com>
2024-11-19 13:44:20 +0100

    Update example (#5)
    - Move RTOS init/start to app_main
    - Update csolution.yml and cproject.yml files

--- RTE/_Debug_MIMXRT1060-EVKB/RTE_Components.h
+++ RTE/_Debug_MIMXRT1060-EVKB/RTE_Components.h
@@ -15,28 +15,28 @@
  */
 #define CMSIS_device_header "fsl_device_registers.h"
 
-/* ARM::CMSIS-Compiler:STDOUT:Custom@1.0.0 */
-#define RTE_CMSIS_Compiler_STDOUT                /* CMSIS-Compiler STDOUT */
-#define RTE_CMSIS_Compiler_STDOUT_Custom         /* CMSIS-Compiler STDOUT: Custom */
-/* ARM::CMSIS:RTOS2:Keil RTX5&Source@5.8.0 */
+/* ARM::CMSIS-Compiler:I/O:STDOUT&User@1.0.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+#define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
+/* ARM::CMSIS:RTOS2:Keil RTX5&Source@5.5.4 */
 #define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
 #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
 #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
-/* NXP::Board Support:SDK Drivers:evkbmimxrt1060:2.0.1 */
+/* NXP::Board Support:SDK Drivers:evkbmimxrt1060@2.0.1 */
 #ifndef XIP_EXTERNAL_FLASH
 #define XIP_EXTERNAL_FLASH 1
 #endif
 #ifndef XIP_BOOT_HEADER_ENABLE
 #define XIP_BOOT_HEADER_ENABLE 1
 #endif
-/* NXP::Device:SDK Drivers:xip_device:2.0.3 */
+/* NXP::Device:SDK Drivers:xip_device@2.0.3 */
 #ifndef XIP_EXTERNAL_FLASH
 #define XIP_EXTERNAL_FLASH 1
 #endif
 #ifndef XIP_BOOT_HEADER_ENABLE
 #define XIP_BOOT_HEADER_ENABLE 1
 #endif
-/* NXP::Device:SDK Utilities:serial_manager_uart:1.0.0 */
+/* NXP::Device:SDK Utilities:serial_manager_uart@1.0.0 */
 #ifndef SERIAL_PORT_TYPE_UART
 #define SERIAL_PORT_TYPE_UART 1
 #endif
